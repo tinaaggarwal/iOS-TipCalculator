372922e146e8c1b57852662d275fd1f682fc7f18
Tina Aggarwal <tina.14a@gmail.com>
2020-01-09 14:21:25 +0530

    Initial Commit

---
+++ iOS-TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-TipCalculator
+//
+//  Created by Tina Aggarwal on 1/9/20.
+//  Copyright © 2020 Tina Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
